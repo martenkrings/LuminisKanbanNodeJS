4c3d21090524d970263015d317433609cb416714
Marten <krings.marten@gmail.com>
2016-10-22 17:25:07 +0200

    user by id

--- resources/userresource.js
+++ resources/userresource.js
@@ -6,6 +6,21 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var User = require('../model/user.js');
 
+router.get('/:userid', function (req, res) {
+    var token = req.header("token");
+    jwt.verify(token, req.app.get('private-key'), function (err, decoded) {
+        if (err) {
+            res.status(401).json({error: 'Forbidden'});
+        }
 
+        User.findOne({_id: req.params.userId}, function (userErr, result) {
+            if (userErr) {
+                res.status(400).json({error: 'Bad Request'});
+            } else {
+                res.status(200).json(result);
+            }
+        });
+    })
+});
 
 module.exports = router;
