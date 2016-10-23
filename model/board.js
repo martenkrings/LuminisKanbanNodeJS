f0c0665e285ce5bb3e4bad34732a44cb0a2df81c
Marten <krings.marten@gmail.com>
2016-10-23 13:54:01 +0200

    role refactor

--- model/board.js
+++ model/board.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var boardSchema = new mongoose.Schema({
     title: {type: String, required: true, unique: true},
     description: {type: String, required: true},
-    dateCreated: {type: Date, default: Date.now()},
+    dateCreated: {type: Date, default: Date.now()}
 });
 
 module.exports = mongoose.model('Board', boardSchema);
